2888fad878a2144904110c4516c0ed78bc973555
eqAshish <ashish.gaur@aieze.in>
2025-05-29 04:03:15 +0530

    committttt

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import React from "react";
 import './App.css';
 import Header from './components/header';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/main';
 
 function App() {
